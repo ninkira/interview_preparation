80ccc74f17b3e0f4b0c04a9f756b42efc4e65e11
ninkira <neckertz921@gmail.com>
2023-03-13 10:15:42 +0100

    Pivot Problem Solution

--- LeetCodeQuestions/LeetWeek_1.py
+++ LeetCodeQuestions/LeetWeek_1.py
@@ -20,3 +20,28 @@ def runningSum(self, nums: List[int]) -> List[int]:
     for i in range(1, len(nums)):
         nums[i] += nums[i - 1]
     return nums
+
+
+def pivotIndex(self, nums: List[int]) -> int:
+    """
+    Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+    created after: https://www.youtube.com/watch?v=u89i60lYx8U
+    structure: build up sum each from left and right 'side' of the list. if they are equal, there is a pivot ('middle'), return the pivot's position
+    if not, return -1, there is no pivot
+    :rtype: object
+    """
+    total_sum = sum(nums)
+    left_sum = 0
+
+    for i in range(len(nums)):
+        right_sum = total_sum - nums[i] - left_sum
+        if left_sum == right_sum: # check if the current position is th pivot index
+            return i # return the index not the actual value
+        left_sum += nums[i]
+    return -1
